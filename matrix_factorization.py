a89b4cb5024ed930859011d0d783cf0da60b36db
Jenny Juarez <jj497@cornell.edu>
2021-12-17 15:01:20 -0500

    add bias

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -8,8 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import collections
-import sklearn
-import sklearn.manifold
+from sklearn.model_selection import train_test_split
 
 
 
@@ -35,12 +34,15 @@ def df_to_tensor_dataset(df):
     return dataset
 
 
-class Model(nn.Module):
-    def __init__(self, num_movies, num_users, embedding_dim):
+class MatrixFactorization(nn.Module):
+    def __init__(self, num_movies, num_users, embedding_dim, std_dev):
         super().__init__()
         self.embedding_dim = embedding_dim
         self.movie_embeddings = nn.Embedding(num_movies, embedding_dim)
         self.user_embeddings = nn.Embedding(num_users, embedding_dim)
+
+        nn.init.normal_(self.movie_embeddings.weight, std = std_dev)
+        nn.init.normal_(self.user_embeddings.weight, std = std_dev)
 
     def forward(self, movies, users):
         batch_size = movies.shape[0]
@@ -57,6 +59,28 @@ class Model(nn.Module):
         return result.view(batch_size, 1)
 
 
+class Scaler(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.bias = nn.Parameter(torch.tensor([0.0]))
+        self.scale = nn.Parameter(torch.tensor([1.0]))
+
+    def forward(self, x):
+        return x * self.scale + self.bias
+
+
+class Model(nn.Module):
+    def __init__(self, num_movies, num_users, embedding_dim, std_dev):
+        super().__init__()
+        self.matrix_factorization = MatrixFactorization(num_movies, num_users, embedding_dim, std_dev)
+        self.scaler = Scaler()
+
+    def forward(self, movies, users):
+        x = self.matrix_factorization(movies, users)
+        x = self.scaler(x)
+        return x
+
+
 def evaluate(dataloader, model, loss_fn):
     num_examples = 0
     total_loss = 0
@@ -64,15 +88,12 @@ def evaluate(dataloader, model, loss_fn):
         for t_movies, t_users, y in dataloader:
             pred = model(t_movies, t_users)
             loss = loss_fn(pred, y.view(-1, 1))
-            total_loss += loss.sum().item() # Sum batch
+            total_loss += loss.sum().item() * t_movies.shape[0]
             num_examples += t_movies.shape[0]
     return total_loss / num_examples
 
 
-
-
-
-def train(train_df, test_df, embedding_dim, batch_size, learning_rate, epochs):
+def train(train_df, test_df, embedding_dim, batch_size, learning_rate, epochs, std_dev, weight_decay):
     train_dataset = df_to_tensor_dataset(train_df)
     test_dataset = df_to_tensor_dataset(test_df)
 
@@ -81,9 +102,16 @@ def train(train_df, test_df, embedding_dim, batch_size, learning_rate, epochs):
     num_users = max(train_df['user_id'].max(), test_df['user_id'].max()) + 1
     # Does traing data contain all the movie Ids
 
-    model = Model(num_movies, num_users, embedding_dim)
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    model = Model(num_movies, num_users, embedding_dim, std_dev)
+
+    print('weight_decay', weight_decay)
+
+    optimizer = torch.optim.SGD([
+        {'params': model.matrix_factorization.parameters()},
+        {'params': model.scaler.parameters(), 'weight_decay': 0}
+    ], lr=learning_rate, weight_decay=weight_decay)
     loss_fn = nn.MSELoss()
+
     train_loader = DataLoader(train_dataset, batch_size=batch_size)
     test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
@@ -99,7 +127,7 @@ def train(train_df, test_df, embedding_dim, batch_size, learning_rate, epochs):
             loss.backward()
             optimizer.step()
 
-            total_loss += loss.sum().item()
+            total_loss += loss.sum().item() * t_movies.shape[0]
             total_examples += t_movies.shape[0]
 
 
@@ -119,8 +147,10 @@ def main():
     parser.add_argument("test_path", help="file path of testing file")
     parser.add_argument("--embedding_dim", help="embedding dimension", type=int, default=100)
     parser.add_argument("--batch_size", help="batch size", type=int, default=1024)
-    parser.add_argument("--learning_rate", help="learning rate", type=float, default=0.001)
+    parser.add_argument("--learning_rate", help="learning rate", type=float, default=0.01)
     parser.add_argument("--epochs", help="epochs", type=int, default=100)
+    parser.add_argument("--std_dev", help="standard deviation", type=int, default=.5)
+    parser.add_argument("--weight_decay", help="weight_decay", type=int, default=.01)
 
     args = parser.parse_args()
     filepath_train = args.train_path
@@ -129,12 +159,21 @@ def main():
     batch_size = args.batch_size
     learning_rate = args.learning_rate
     epochs = args.epochs
+    std_dev = args.std_dev
+    weight_decay = args.weight_decay
 
     # 2. Read data
     train_df = read_data(args.train_path)
-    test_df = read_data(args.test_path)
+    print('full train len', len(train_df))
+    if args.train_path == args.test_path:
+        X_train, X_test = train_test_split(train_df, test_size=0.1)
+        train_df = X_train
+        test_df = X_test
+    else:
+        test_df = read_data(args.test_path)
 
-    train(train_df, test_df, embedding_dim, batch_size, learning_rate, epochs)
+    print('split train len', len(train_df))
+    train(train_df, test_df, embedding_dim, batch_size, learning_rate, epochs, std_dev, weight_decay)
 
 
 
